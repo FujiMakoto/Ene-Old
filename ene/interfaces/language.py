3a6c7df261bcd1064425a57ab99f6af82f82a40c
Makoto <FujiMakoto@users.noreply.github.com>
2015-09-02 20:48:33 -0400

    Interface boilerplate

---
+++ ene/interfaces/language.py
@@ -0,0 +1,13 @@
+from abc import ABCMeta, abstractmethod
+
+
+class LanguageManager:
+    pass
+
+
+class LanguageFactory:
+    pass
+
+
+class LanguageAbstract(metaclass=ABCMeta):
+    pass
---
+++ ene/interfaces/protocol.py
@@ -0,0 +1,13 @@
+from abc import ABCMeta, abstractmethod
+
+
+class ProtocolManager:
+    pass
+
+
+class ProtocolFactory:
+    pass
+
+
+class ProtocolAbstract(metaclass=ABCMeta):
+    pass
